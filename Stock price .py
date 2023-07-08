74177c1933443edc950310641d1f4a0d3ae8a090
Mousumi Nayak <71492985+mousuminayak@users.noreply.github.com>
2023-07-08 13:17:07 +0530

    Add files via upload

---
+++ Stock price .py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import chart_studio.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+#for offline plotting
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+
+
+# In[ ]:
+
+
+
+
